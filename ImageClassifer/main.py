f3e8a766722c5609db96ef930e8785e5466b0881
Hy-LeTuan <letuanhy1507@gmail.com>
2025-03-23 19:10:39 -0500

    some changes

--- ImageClassifer/main.py
+++ ImageClassifer/main.py
@@ -1,8 +1,1 @@
-print("hello, world")
-print("hello, world")
-print("hello, world")
-print("hello, world")
-print("hello, world")
-
-# askldfj;laskdjf;lasdjf;alsdkjfa;lsdkjf;ladskjf;laskdfj
-# asdlkjf;alsjfal;dskjf;alsdkj
+# some changes
