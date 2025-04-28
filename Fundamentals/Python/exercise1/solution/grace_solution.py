4313a4ee5b05e761455cd1feea9c236fbdfcd32f
Grace <gd@Graces-MacBook-Pro.local>
2025-04-27 20:12:44 -0500

    type something meaningful

---
+++ Fundamentals/Python/exercise1/solution/grace_solution.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+age = int(input("What is your age?"))
+year_age_is_100 = 2025 + (100 - age)
+
+print("Hello" , name ,  ", you will be 100 years old in" , year_age_is_100 , ".")
