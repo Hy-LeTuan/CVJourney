c5fd726fd623ce94c11f0ce04ccc09e0e6faa5fa
Deku <linmyathu195@gmail.com>
2025-05-12 12:29:23 -0500

    complete

---
+++ Fundamentals/Python/exercise2/solution/lin_solution.py
@@ -0,0 +1,51 @@
+import datetime
+
+watchable_movies = []
+today = datetime.datetime.today()
+
+def check_input(input_type):
+    while True: 
+        try:
+            if input_type == "date":
+                user_birth_date = int(input("Type your birth date: "))
+                if user_birth_date > 31 or user_birth_date <= 0:
+                    print("Enter only valid Birthdate")
+                    continue
+                return user_birth_date
+            
+            elif input_type == "month":
+                user_birth_month = int(input("Type your birth month: "))
+                if user_birth_month > 12 or user_birth_month <= 0:
+                    print("Enter only valid Birth Month")
+                    continue
+                return user_birth_month
+            
+            elif input_type == "year":
+                user_birth_year = int(input("Type your brith year: "))
+                if user_birth_year <= 0 or user_birth_year > today.year:
+                    print("Enter only valid Birth Year")
+                    continue
+                return user_birth_year
+        except:
+            print("Enter only valid Answer")
+            
+            
+user_birth_date = check_input("date")
+user_birth_month = check_input("month")
+user_birth_year = check_input("year")
+
+birthday_obj = datetime.datetime(user_birth_year, user_birth_month, user_birth_date)
+        
+current_age = (today - birthday_obj).days / 365
+#there is a problem here with leap years with 365 days 
+
+if current_age > 17:
+    watchable_movies.extend(["R", "PG", "PG-13"])
+
+elif current_age <= 17 and current_age > 12:
+    watchable_movies.extend(["PG", "PG-13"])
+else:
+    watchable_movies.append("PG")
+
+print(f'Age: {current_age}')
+print(f'Watchable Movies: {watchable_movies}')
