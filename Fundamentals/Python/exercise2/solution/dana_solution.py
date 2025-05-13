555aca79e8f69627df9846747879b0d443acac28
Ranzo <danamg652006@gmail.com>
2025-05-12 22:31:31 -0500

    Solved the exercise 2 and the Extra using steps from exercise 1

---
+++ Fundamentals/Python/exercise2/solution/dana_solution.py
@@ -0,0 +1,44 @@
+import datetime
+
+categories_list = ["PG", "PG-13", "R"]
+
+while True:
+    try:
+
+        day = int(input("Enter your day of birth: "))
+        month = int(input("Enter your month of birth: "))
+        year = int(input("Enter your year of birth: "))
+
+        birth_date = datetime.date(year, month, day)
+        now = datetime.date.today() 
+
+        if birth_date > now:
+            print("Birth date error. Please try again.\n")
+            continue
+        
+        age = now.year - birth_date.year
+
+        if now.month < month:
+           age -= 1
+        elif now.month == month:
+            if now.day < day:
+                age -= 1
+
+
+
+        print("Movie categories:", categories_list)
+        print(age)
+
+        if age >= 17:
+            print("You can watch R-rated movies")
+        elif age >= 13:
+            print("You can watch PG-13 movies")
+        elif age <= 12:
+            print("You can watch PG movies")
+                
+        break 
+
+    except ValueError:
+        print("Invalid input. Please enter valid numbers for day, month, and year.\n")
+    
+
