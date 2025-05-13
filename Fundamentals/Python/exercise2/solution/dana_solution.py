1bbf955dfcd21e6a79c033fe48b7d97f7555a028
Ranzo <danamg652006@gmail.com>
2025-05-13 15:58:41 -0500

    add the line for the year restrictions and changed the watchable movies list

--- Fundamentals/Python/exercise2/solution/dana_solution.py
+++ Fundamentals/Python/exercise2/solution/dana_solution.py
@@ -12,6 +12,9 @@ while True:
         birth_date = datetime.date(year, month, day)
         now = datetime.date.today() 
 
+        if year < now.year - 100:
+            print("Birth date error. Please try again.\n")
+            continue
         if birth_date > now:
             print("Birth date error. Please try again.\n")
             continue
@@ -30,9 +33,9 @@ while True:
         print(age)
 
         if age >= 17:
-            print("You can watch R-rated movies")
+            print("You can watch R-rated, PG-13, PG movies")
         elif age >= 13:
-            print("You can watch PG-13 movies")
+            print("You can watch PG-13 and PG movies")
         elif age <= 12:
             print("You can watch PG movies")
                 
