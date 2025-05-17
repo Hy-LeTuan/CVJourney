2c2ef1bef3ec1a8342c8811d740722bd6ee5d409
Deku <linmyathu195@gmail.com>
2025-05-17 10:46:35 -0500

    fixed repeating code and main function

--- Fundamentals/Python/exercise2/solution/lin_solution.py
+++ Fundamentals/Python/exercise2/solution/lin_solution.py
@@ -9,43 +9,50 @@ def check_input(input_type):
             if input_type == "date":
                 user_birth_date = int(input("Type your birth date: "))
                 if user_birth_date > 31 or user_birth_date <= 0:
-                    print("Enter only valid Birthdate")
-                    continue
+                    raise ValueError ("You entered invalid birthday!")
                 return user_birth_date
             
             elif input_type == "month":
                 user_birth_month = int(input("Type your birth month: "))
                 if user_birth_month > 12 or user_birth_month <= 0:
-                    print("Enter only valid Birth Month")
-                    continue
+                    raise ValueError ("You entered invalid month of birth")
                 return user_birth_month
             
             elif input_type == "year":
                 user_birth_year = int(input("Type your brith year: "))
-                if user_birth_year <= 0 or user_birth_year > today.year:
-                    print("Enter only valid Birth Year")
-                    continue
+                if abs(user_birth_year - (today.year)) > 200:
+                    raise ValueError ("You entered invalid year of birth")
                 return user_birth_year
+            
+        except ValueError as e:
+            print(e)
+            
         except:
             print("Enter only valid Answer")
+
             
+def main():
+    user_birth_date = check_input("date")
+    user_birth_month = check_input("month")
+    user_birth_year = check_input("year")
+
+    birthday_obj = datetime.datetime(user_birth_year, user_birth_month, user_birth_date)
             
-user_birth_date = check_input("date")
-user_birth_month = check_input("month")
-user_birth_year = check_input("year")
+    current_age = (today - birthday_obj).days / 365
+    #there is a problem here with leap years with 365 days 
 
-birthday_obj = datetime.datetime(user_birth_year, user_birth_month, user_birth_date)
-        
-current_age = (today - birthday_obj).days / 365
-#there is a problem here with leap years with 365 days 
+    if current_age > 17:
+        watchable_movies.extend(["R", "PG", "PG-13"])
 
-if current_age > 17:
-    watchable_movies.extend(["R", "PG", "PG-13"])
+    elif current_age <= 17 and current_age > 12:
+        watchable_movies.extend(["PG", "PG-13"])
+    else:
+        watchable_movies.append("PG")
 
-elif current_age <= 17 and current_age > 12:
-    watchable_movies.extend(["PG", "PG-13"])
-else:
-    watchable_movies.append("PG")
+    print(f'Age: {current_age}')
+    print(f'Watchable Movies: {watchable_movies}')
+    return
 
-print(f'Age: {current_age}')
-print(f'Watchable Movies: {watchable_movies}')
+if __name__ == "__main__":
+    main()
+
